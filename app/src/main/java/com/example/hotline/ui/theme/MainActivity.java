218bcb39cc5b87ec00ba30187d26fb0e51d9e093
AplhaA <133354992+AplhaA@users.noreply.github.com>
2023-05-14 17:06:58 +0800

    4rth

---
+++ app/src/main/java/com/example/hotline/ui/theme/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.hotline.ui.theme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hotline.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/hotline/ui/theme/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.example.hotline.ui.theme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hotline.R;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
